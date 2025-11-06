78dbd112cca1796f6028c6b10333e9141859b8b8
corentin le bris <corentin.lebris@hotmail.fr>
2025-11-06 17:02:15 +0100

    add config for external database

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,8 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ log: ["query"] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
